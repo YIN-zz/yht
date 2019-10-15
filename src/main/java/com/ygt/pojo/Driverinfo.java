0379c8ae25417902b5c44bfb4598a7e698ee3e76
YIN <1058397245@qq.com>
2019-10-15 09:36:57 +0800

    222

---
+++ src/main/java/com/ygt/pojo/Driverinfo.java
@@ -0,0 +1,50 @@
+package com.ygt.pojo;
+
+import lombok.*;
+import org.joda.time.DateTime;
+
+
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Driverinfo {
+    //出入库id
+    private int drid;
+    //备案证号
+    private String recordid;
+    //出入库时间
+    private DateTime rtime;
+    //货车司机名字
+    private String rdriver;
+    //手机号
+    private int rphone;
+    //车牌号
+    private String rnumber;
+    //备案号或者许可证照片
+    private String recordphoto;
+    //驾驶证或行驶证照片
+    private String driverphoto;
+    //司机站在货车前照片
+    private String carphoto;
+    //货品用途
+    private String ruse;
+    //货品出入库
+    private String rinout;
+    //仓库管理员id
+    private int sid;
+    //易制毒化学品id
+    private int chid;
+    //易制毒设备id
+    private int fid;
+
+
+
+
+
+
+
+
+
+}
