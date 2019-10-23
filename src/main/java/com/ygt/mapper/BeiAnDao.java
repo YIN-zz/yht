123da2afadbfd7c02f71c8919f5a5aa30d21c0e9
YIN <1058397245@qq.com>
2019-10-23 13:25:09 +0800

    di san  ci

--- src/main/java/com/ygt/mapper/BeiAnDao.java
+++ src/main/java/com/ygt/mapper/BeiAnDao.java
@@ -8,4 +8,6 @@ public interface BeiAnDao {
     //添加出入库货物的信息
     @Insert("insert into beian (recordid, bname, bcwerght, bccount,chid) values(#{recordid},#{bname},#{bcwerght},#{bccount},#{chid})")
     int addBeiAn(BeiAn beiAn);
+
+
 }
