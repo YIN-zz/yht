f3012503d6c6f6230ada58ee8644a3e293992d06
YIN <1058397245@qq.com>
2019-10-23 13:56:14 +0800

    di san  ci

--- src/main/java/com/ygt/mapper/GoodsinfoDao.java
+++ src/main/java/com/ygt/mapper/GoodsinfoDao.java
@@ -1,4 +1,11 @@
 package com.ygt.mapper;
 
-public class GoodsinfoDao {
+import com.ygt.pojo.Goodsinfo;
+
+public interface GoodsinfoDao {
+    //添加出入库商品
+    int addBeiAn(Goodsinfo goodsinfo);
+
+
+
 }
